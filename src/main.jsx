251649a3415f7fad725370bdfb66cd71a32e0ec4
cafagnarob <cafagna.rob@gmail.com>
2026-07-23 14:43:41 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,7 @@ import "./index.css"
 import App from "./App.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-<<<<<<< HEAD
 createRoot(document.getElementById("root")).render(
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-createRoot(document.getElementById('root')).render(
->>>>>>> develop
   <StrictMode>
     <BrowserRouter>
       <App />
