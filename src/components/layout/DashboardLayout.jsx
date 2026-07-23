8bc4c900e606773a1441351265f382b482699cf7
cafagnarob <cafagna.rob@gmail.com>
2026-07-23 13:26:02 +0200

    Update DashboardLayout.jsx

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -1,36 +1,33 @@
 // importo Outlet da React Router
 // Outlet rappresenta la pagina interna da visualizzare
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom"
 
 // importo i componenti del layout
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import Sidebar from "./Sidebar"
+import Navbar from "./Navbar"
 
 // importo il CSS
-import "./DashboardLayout.css";
+import "./DashboardLayout.css"
 
 const DashboardLayout = () => {
   return (
     // contenitore principale della dashboard
     <div className="dashboard-layout">
-
       {/* barra laterale visibile in tutte le pagine */}
       <Sidebar />
 
       {/* parte destra della dashboard */}
       <div className="dashboard-content">
-
         {/* barra superiore visibile in tutte le pagine */}
         <Navbar />
 
         {/* qui verrà mostrata la pagina selezionata */}
-        <main className="dashboard-main">
+        <main className="dashboard-main, p-0, m-0, mt-3">
           <Outlet />
         </main>
-
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DashboardLayout;
+export default DashboardLayout
