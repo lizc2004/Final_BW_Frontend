cf145601ccbf5042156dfb2d19cf45f9ceab06bd
cafagnarob <cafagna.rob@gmail.com>
2026-07-23 11:38:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from './components/pages/Login/Login'
-import Panoramica from './components/pages/Panoramica/Panoramica'
-import Clienti from './components/pages/Clienti/Clienti'
-import Fatture from './components/pages/Fatture/Fatture'
-import StatiFattura from './components/pages/StatiFattura/StatiFattura'
-import ContattoClienti from './components/pages/ContattoClienti/ContattoClienti'
-import Utenti from './components/pages/Utenti/Utenti'
+import { Routes, Route } from "react-router-dom"
+import Login from "./components/pages/Login/Login"
+import Panoramica from "./components/pages/Panoramica/Panoramica"
+import Clienti from "./components/pages/Clienti/Clienti"
+import Fatture from "./components/pages/Fatture/Fatture"
+import StatiFattura from "./components/pages/StatiFattura/StatiFattura"
+import ContattoClienti from "./components/pages/ContattoClienti/ContattoClienti"
+import Utenti from "./components/pages/Utenti/Utenti"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Panoramica />} />
-      <Route path="/clienti" element={<Clienti />} />
-      <Route path="/fatture" element={<Fatture />} />
-      <Route path="/stati-fattura" element={<StatiFattura />} />
-      <Route path="/invia-email" element={<ContattoClienti />} />
-      <Route path="/utenti" element={<Utenti />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/login" element={<Login />} />
+    //   <Route path="/" element={<Panoramica />} />
+    //   <Route path="/clienti" element={<Clienti />} />
+    //   <Route path="/fatture" element={<Fatture />} />
+    //   <Route path="/stati-fattura" element={<StatiFattura />} />
+    //   <Route path="/invia-email" element={<ContattoClienti />} />
+    //   <Route path="/utenti" element={<Utenti />} />
+    // </Routes>
+
+    <UtentiTest />
   )
 }
 
