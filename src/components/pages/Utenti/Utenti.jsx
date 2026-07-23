35daff55fbd797850004ff9e3cf2656e83ccefe4
cafagnarob <cafagna.rob@gmail.com>
2026-07-23 13:26:05 +0200

    Update Utenti.jsx

--- src/components/pages/Utenti/Utenti.jsx
+++ src/components/pages/Utenti/Utenti.jsx
@@ -3,6 +3,7 @@ import "./Utenti.css"
 import { useEffect, useState } from "react"
 import { getMyProfile } from "../../../Api/utenteApi"
 import { updateMe } from "../../../Api/utenteApi"
+import { Col, Container, Row, Table } from "react-bootstrap"
 
 export default function Profilo() {
   const [utente, setUtente] = useState(null)
@@ -24,32 +25,74 @@ export default function Profilo() {
   }
   return (
     <>
-      <div className="bg-white">
-        <h1>Utenti</h1>
-        <p>Gestione utenti e ruoli</p>
-      </div>
-      <div>
-        {utente && (
-          <>
-            <p>
-              Username:
-              {utente.username}
-            </p>
+      <Container fluid className="mw-100">
+        <Row className="bg-white">
+          <Col>
+            <div>
+              <h1>Utenti</h1>
+              <p>Gestione utenti e ruoli</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid className="bg-primary">
+        <div>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>UTENTE</th>
+                <th>USERNAME</th>
+                <th>EMAIL</th>
+                <th>RUOLI</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <p>
+                    Nome:
+                    {utente.nome}
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    Username:
+                    {utente.username}
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    Email:
+                    {utente.email}
+                  </p>
+                </td>
+                <td>@mdo</td>
+              </tr>
+            </tbody>
+          </Table>
 
-            <p>
-              Email:
-              {utente.email}
-            </p>
+          {utente && (
+            <>
+              <p>
+                Username:
+                {utente.username}
+              </p>
 
-            <p>
-              Nome:
-              {utente.nome}
-            </p>
+              <p>
+                Email:
+                {utente.email}
+              </p>
 
-            <button onClick={modifica}>Modifica nome</button>
-          </>
-        )}
-      </div>
+              <p>
+                Nome:
+                {utente.nome}
+              </p>
+
+              <button onClick={modifica}>Modifica nome</button>
+            </>
+          )}
+        </div>
+      </Container>
     </>
   )
 }
