5c1d5f49aeef381d1481d30312d10369e2fe25a3
cafagnarob <cafagna.rob@gmail.com>
2026-07-23 13:25:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom"
 
 // importo il layout comune della dashboard
-import DashboardLayout from "./components/layout/DashboardLayout";
+import DashboardLayout from "./components/layout/DashboardLayout"
 
-import Login from "./components/pages/Login/Login";
-import Panoramica from "./components/pages/Panoramica/Panoramica";
-import Clienti from "./components/pages/Clienti/Clienti";
-import Fatture from "./components/pages/Fatture/Fatture";
-import StatiFattura from "./components/pages/StatiFattura/StatiFattura";
-import ContattoClienti from "./components/pages/ContattoClienti/ContattoClienti";
+import Login from "./components/pages/Login/Login"
+import Panoramica from "./components/pages/Panoramica/Panoramica"
+import Clienti from "./components/pages/Clienti/Clienti"
+import Fatture from "./components/pages/Fatture/Fatture"
+import StatiFattura from "./components/pages/StatiFattura/StatiFattura"
+import ContattoClienti from "./components/pages/ContattoClienti/ContattoClienti"
+import Utenti from "../src/components/pages/Utenti/Utenti"
 
 function App() {
   return (
@@ -22,12 +23,13 @@ function App() {
         <Route path="fatture" element={<Fatture />} />
         <Route path="stati-fattura" element={<StatiFattura />} />
         <Route path="invia-email" element={<ContattoClienti />} />
+        <Route path="utenti" element={<Utenti />} />
       </Route>
 
       {/* Se il percorso non esiste, torna alla panoramica */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
